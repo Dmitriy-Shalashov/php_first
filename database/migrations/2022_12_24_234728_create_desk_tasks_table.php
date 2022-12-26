7fccb5bed3c24b8bdd7858bbef062c21fed35993
Dmitriy Shalashov <dmitriy.tackle@gmail.com>
2022-12-26 13:19:33 +0200

    start pack

---
+++ database/migrations/2022_12_24_234728_create_desk_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('desk_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('desk_card_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('desk_tasks');
+    }
+};
